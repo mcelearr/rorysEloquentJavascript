21d0a1b8a5fd739613527d2a04315becfe248620
mcelearr <rorymcelearney@gmail.com>
2016-05-11 09:00:27 +0100

    start to creat world object

--- chapter-7/electronic-life.js
+++ chapter-7/electronic-life.js
@@ -63,5 +63,25 @@ function BouncingCritter() {
 BouncingCritter.prototype.act = function(view) {
   if (view.look.(this.direction) != " ")
     this.direction = view.find(" ") || "s";
-  return {type:"move",direction: this.direction};
+  return {type:"move", direction: this.direction};
 };
+
+function elementFromChar(legend, ch) {
+  if (ch == " ")
+    return null;
+  var element = new legend[ch]();
+  element.originChar = ch;
+  return element;
+}
+
+function World(map, legend) {
+  var grid = new Grid(map[0].length, map.length);
+  this.grid = grid;
+  this.legend = legend;
+
+  map.forEach(function(line, y) {
+    for (var x = 0; x < line.length; x++)
+      grid.set(new Vector(x,y),
+              elementFromChar(legend,line[x]));
+  });
+}
