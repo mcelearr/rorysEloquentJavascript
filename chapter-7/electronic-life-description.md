087f6548121113613b2e0fcbefffbd2b65b6a41e
mcelearr <rorymcelearney@gmail.com>
2016-05-11 08:44:20 +0100

    Critter's Programming Interface

--- chapter-7/electronic-life-description.md
+++ chapter-7/electronic-life-description.md
@@ -1,3 +1,3 @@
-Chapter 7 exercise from Eloquent JS. Possible further projects:
+Chapter 7 exercise from Marijn Haverbeke's Eloquent JS. Possible further projects:
 * Rewrite from object to functional style.
-* Create visual representation and host on personal website. 
+* Create visual representation and host on personal website.
--- chapter-7/electronic-life.js
+++ chapter-7/electronic-life.js
@@ -38,3 +38,30 @@ Grid.prototype.get = function(vector) {
 Grid.prototype.set = function(vector, value) {
   this.space[vector.x + this.width * vector.y] = value;
 };
+
+var directions = {
+  "n":  new Vector( 0, -1),
+  "ne": new Vector( 1, -1),
+  "e":  new Vector( 1,  0),
+  "se": new Vector( 1,  1),
+  "s":  new Vector( 0,  1),
+  "sw": new Vector(-1,  1),
+  "w":  new Vector(-1,  0),
+  "nw": new Vector(-1, -1)
+};
+
+function randomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+var directionNames = ["n","ne","e","se","s","sw","w","nw"];
+
+function BouncingCritter() {
+  this.direction = randomElement(directionNames);
+}
+
+BouncingCritter.prototype.act = function(view) {
+  if (view.look.(this.direction) != " ")
+    this.direction = view.find(" ") || "s";
+  return {type:"move",direction: this.direction};
+};
